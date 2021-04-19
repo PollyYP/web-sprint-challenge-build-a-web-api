761815a119715dc92e7bb07fa83df70952be074c
Polly Yospan <porgroovy@gmail.com>
2021-04-18 23:48:01 -0400

    MVP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 const server = express();
+const helmet = require("helmet");
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+
+const projectRouter = require("./projects/projects-router");
+const actionRouter = require("./actions/actions-router");
+
+server.use(helmet());
+server.use(express.json());
+server.use(projectRouter);
+server.use(actionRouter);
+
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
 
 module.exports = server;
